d29ab41a12c22ff34c64ece03c3d88d9befe62ce
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-01-24 13:23:32 +0100

    Add files via upload

---
+++ api.php
@@ -0,0 +1,98 @@
+
+<?PHP
+
+	header("Content-Type:application/json");
+
+	$verb = $_SERVER['REQUEST_METHOD'];
+	$my_file = 'file.txt';
+	$messages = json_decode(openFile($my_file));
+	$i = count($messages);
+
+	//var_dump($_GET['lastid']);
+
+	if ($verb == "GET") {
+
+		if (isset($_GET['lastid']) and isset($_GET['mykey'])) {
+			global $messages;
+			$getMessage = $messages[$_GET['lastid']];
+			response($getMessage[0], $getMessage[1], $getMessage[2]);
+
+		} else {
+
+			http_response_code(400);
+
+		}
+
+	} elseif ($verb == "PUT") {
+
+		if (isset($_GET['mykey']) and isset($_GET['value'])) {
+
+			$newMessage = array($i, $_GET['mykey'], $_GET['value']);
+			array_push($messages, $newMessage);
+			writeFile($my_file, $messages);
+			http_response_code(200);
+
+			echo("Message stored");
+
+		} else {
+
+			http_response_code(400);
+
+		}
+
+	} else {
+
+		http_response_code(400);
+
+	}
+
+	function openFile($file) {
+		$handle = fopen($file, 'r');
+		return fread($handle,filesize($file));
+	}
+
+	function writeFile($file, $message) {
+		$message = json_encode($message);
+		$handle = fopen($file, 'w');
+		fwrite($handle, $message);
+	}
+
+	function response($id, $key, $message) {
+		header("HTTP/1.1 ");
+		
+		$response['message'] = $message;
+		$response['id'] = $id;
+		$response['key'] = $key;
+		
+		$json_response = json_encode($response);
+		echo $json_response;
+	}
+
+/*
+	if($verb == "GET") {
+		if(isset($_GET['action']) and isset($_GET['mykey'])) {
+			$name = $_GET['mykey'];
+			response($name);
+		} else {
+			echo "error: please enter required parameters";
+		}
+	} elseif ($verb == "PUT") {
+		$handle = fopen($my_file, 'w');
+		$data = 'This is the data as well';
+		fwrite($handle, $data);
+		$handle = fopen($my_file, 'r');
+		response(fread($handle,filesize($my_file)));
+	} else {
+		echo "error: verb unknown";
+	}
+
+	function response($name) {
+		header("HTTP/1.1 ");
+		
+		$Response = $name;
+		
+		$json_response = $Response;
+		echo $json_response;
+	}
+*/
+?>
